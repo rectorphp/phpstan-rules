2f6afc72d7cbc82f77b177f77dc52811db033b6f
Tomas Votruba <tomas.vot@gmail.com>
2021-05-12 19:11:23 +0200

    Add ForbiddenInterfacesTogetherRule (#4)

---
+++ tests/Rule/ForbiddenInterfacesTogetherRule/Fixture/SkipSeparated.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\PHPStanRules\Tests\Rule\ForbiddenInterfacesTogetherRule\Fixture;
+
+use Rector\Core\Contract\Rector\RectorInterface;
+
+final class SkipSeparated implements RectorInterface
+{
+}
