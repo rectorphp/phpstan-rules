5f327b273168c5c22c901b232fc4fd5eaa574dcc
Tomas Votruba <tomas.vot@gmail.com>
2021-05-29 23:48:55 +0100

    Add CallConfigureWithArrayRule (#6)

---
+++ tests/Rule/CallConfigureWithArrayRule/CallConfigureWithArrayRuleTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\PHPStanRules\Tests\Rule\CallConfigureWithArrayRule;
+
+use Iterator;
+use PHPStan\Rules\Rule;
+use Rector\PHPStanRules\Rule\CallConfigureWithArrayRule;
+use Symplify\PHPStanExtensions\Testing\AbstractServiceAwareRuleTestCase;
+
+/**
+ * @extends \Symplify\PHPStanExtensions\Testing\AbstractServiceAwareRuleTestCase<CallConfigureWithArrayRule>
+ */
+final class CallConfigureWithArrayRuleTest extends AbstractServiceAwareRuleTestCase
+{
+    /**
+     * @dataProvider provideData()
+     * @param array<string|string[]|int[]> $expectedErrorsWithLines
+     */
+    public function testRule(string $filePath, array $expectedErrorsWithLines): void
+    {
+        $this->analyse([$filePath], $expectedErrorsWithLines);
+    }
+
+    public function provideData(): Iterator
+    {
+        yield [__DIR__ . '/Fixture/SkipCorrectConfig.php', []];
+        yield [__DIR__ . '/Fixture/WrongConfig.php', [[CallConfigureWithArrayRule::ERROR_MESSAGE, 12]]];
+        yield [__DIR__ . '/Fixture/WrongConfigTooMany.php', [[CallConfigureWithArrayRule::ERROR_MESSAGE, 12]]];
+    }
+
+    protected function getRule(): Rule
+    {
+        return $this->getRuleFromConfig(
+            CallConfigureWithArrayRule::class,
+            __DIR__ . '/config/configured_rule.neon'
+        );
+    }
+}
---
+++ tests/Rule/CallConfigureWithArrayRule/Fixture/WrongConfigTooMany.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\PHPStanRules\Tests\Rule\CallConfigureWithArrayRule\Fixture;
+
+use SomeClass;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+    $services->set(SomeClass::class)
+        ->call('configure', [[[
+            'value',
+        ]]]);
+};
