dd3b5bda2fd83ae2b50af5ec7c8d8915e440ba86
Tomas Votruba <tomas.vot@gmail.com>
2021-09-30 14:06:58 +0200

    preload fial

--- rector.php
+++ rector.php
@@ -2,10 +2,21 @@
 
 declare(strict_types=1);
 
+use Rector\Core\Configuration\Option;
 use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
+use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
     $services->set(ClassPropertyAssignToConstructorPromotionRector::class);
+
+    $containerConfigurator->import(SetList::PHP_74);
+    $containerConfigurator->import(SetList::PHP_80);
+
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PATHS, [
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
 };
