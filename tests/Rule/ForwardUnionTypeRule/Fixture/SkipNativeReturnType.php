8561ed752175d4d4f464c3a17e4c12c8523e5555
Tomas Votruba <tomas.vot@gmail.com>
2021-11-02 11:48:47 +0100

    trigger CI

--- tests/Rule/ForwardUnionTypeRule/Fixture/SkipNativeReturnType.php
+++ tests/Rule/ForwardUnionTypeRule/Fixture/SkipNativeReturnType.php
@@ -6,9 +6,6 @@ namespace Rector\PHPStanRules\Tests\Rule\ForwardUnionTypeRule\Fixture;
 
 final class SkipNativeReturnType
 {
-    /**
-     * @return int|string
-     */
     public function run(int | string $value): int | string
     {
     }
--- tests/Rule/ForwardUnionTypeRule/Fixture/SkipNativeType.php
+++ tests/Rule/ForwardUnionTypeRule/Fixture/SkipNativeType.php
@@ -6,9 +6,6 @@ namespace Rector\PHPStanRules\Tests\Rule\ForwardUnionTypeRule\Fixture;
 
 final class SkipNativeType
 {
-    /**
-     * @param int|string $value
-     */
     public function run(int | string $value)
     {
     }
