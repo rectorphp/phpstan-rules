d1cbc627d4cfa0358a1ebe1dff7f66ef2b1a8d7b
Fabien Villepinte <fabien.villepinte@gmail.com>
2021-11-30 12:03:57 +0100

    [Rule] Add NoLeadingBackslashInNameRule

---
+++ src/Rule/NoLeadingBackslashInNameRule.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\PHPStanRules\Rule;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\New_;
+use PhpParser\Node\Name;
+use PhpParser\Node\Name\FullyQualified;
+use PhpParser\Node\Name\Relative;
+use PHPStan\Analyser\Scope;
+use PHPStan\Type\Constant\ConstantStringType;
+use Symplify\Astral\Naming\SimpleNameResolver;
+use Symplify\PHPStanRules\Rules\AbstractSymplifyRule;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Rector\PHPStanRules\Tests\Rule\NoLeadingBackslashInNameRule\NoLeadingBackslashInNameRuleTest
+ */
+final class NoLeadingBackslashInNameRule extends AbstractSymplifyRule
+{
+    /**
+     * @var string
+     */
+    public const ERROR_MESSAGE = 'Instead of "new Name(\'\\\\Foo\')" use "new FullyQualified(\'Foo\')"';
+
+    public function __construct(
+        private SimpleNameResolver $simpleNameResolver
+    ) {
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [New_::class];
+    }
+
+    /**
+     * @param New_ $node
+     * @return string[]
+     */
+    public function process(Node $node, Scope $scope): array
+    {
+        if ($node->args === []) {
+            return [];
+        }
+
+        $className = $this->simpleNameResolver->getName($node->class);
+        if (! in_array($className, [Name::class, FullyQualified::class, Relative::class], true)) {
+            return [];
+        }
+
+        $argValue = $node->args[0]->value;
+        $argType = $scope->getType($argValue);
+
+        if (! $argType instanceof ConstantStringType) {
+            return [];
+        }
+
+        if (! str_starts_with($argType->getValue(), '\\')) {
+            return [];
+        }
+
+        return [self::ERROR_MESSAGE];
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(self::ERROR_MESSAGE, [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+new Name('\\Closure');
+CODE_SAMPLE
+            ,
+                <<<'CODE_SAMPLE'
+new FullyQualified('Closure');
+CODE_SAMPLE
+            ),
+        ]);
+    }
+}
