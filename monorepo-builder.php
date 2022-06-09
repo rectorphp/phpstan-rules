29e63f5f73327788abb49b63d3eaa671736e809c
Tomas Votruba <tomas.vot@gmail.com>
2022-06-10 01:30:16 +0200

    Bump to Symplify 10.3 (#29)
    * try symplify dev
    
    * config

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -2,16 +2,14 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\MonorepoBuilder\Config\MBConfig;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $services = $containerConfigurator->services();
-    $services->defaults()
-        ->autowire();
-
-    // @see https://github.com/symplify/monorepo-builder#6-release-flow
-    $services->set(TagVersionReleaseWorker::class);
-    $services->set(PushTagReleaseWorker::class);
+return static function (MBConfig $mbConfig): void {
+    $mbConfig->workers([
+        // @see https://github.com/symplify/monorepo-builder#6-release-flow
+        TagVersionReleaseWorker::class,
+        PushTagReleaseWorker::class,
+    ]);
 };
