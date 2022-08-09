195fff64081fa4930757857b9883c7e3290dcc4d
Tomas Votruba <tomas.vot@gmail.com>
2022-08-09 15:01:29 +0200

    add NameResolverReturnTypeExtensionTest with fales positive

---
+++ tests/ReturnTypeExtension/NameResolverReturnTypeExtension/NameResolverReturnTypeExtensionTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\PHPStanRules\Tests\ReturnTypeExtension\NameResolverReturnTypeExtension;
+
+use PHPStan\Testing\TypeInferenceTestCase;
+
+final class NameResolverReturnTypeExtensionTest extends TypeInferenceTestCase
+{
+    /**
+     * @return iterable<string, mixed[]>
+     */
+    public function dataAsserts(): iterable
+    {
+        yield from $this->gatherAssertTypes(__DIR__ . '/data/get_name_class_method.php.inc');
+    }
+
+    /**
+     * @dataProvider dataAsserts()
+     */
+    public function testAsserts(string $assertType, string $file, mixed ...$args): void
+    {
+        $this->assertFileAsserts($assertType, $file, ...$args);
+    }
+
+    /**
+     * @return string[]
+     */
+    public static function getAdditionalConfigFiles(): array
+    {
+        return [__DIR__ . '/../../../config/extensions.neon'];
+    }
+}
