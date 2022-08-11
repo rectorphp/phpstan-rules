100120d0d331785e847bd08b59c205adbe291698
Tomas Votruba <tomas.vot@gmail.com>
2022-08-11 16:11:53 +0200

    allow PHP 7.4 for eaiser install on other projects

--- rector.php
+++ rector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
 use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
+use Rector\Set\ValueObject\DowngradeLevelSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
@@ -12,7 +13,8 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(ClassPropertyAssignToConstructorPromotionRector::class);
 
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_80,
+        //LevelSetList::UP_TO_PHP_80,
+        DowngradeLevelSetList::DOWN_TO_PHP_74,
         SetList::CODE_QUALITY,
         SetList::DEAD_CODE,
         SetList::NAMING
