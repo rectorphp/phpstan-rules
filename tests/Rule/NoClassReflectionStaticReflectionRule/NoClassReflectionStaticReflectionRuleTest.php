0aefdb525e6cf44d6f8f91672d5f887bdad9b88b
Tomas Votruba <tomas.vot@gmail.com>
2022-12-23 16:03:31 +0100

    inline

--- tests/Rule/NoClassReflectionStaticReflectionRule/NoClassReflectionStaticReflectionRuleTest.php
+++ tests/Rule/NoClassReflectionStaticReflectionRule/NoClassReflectionStaticReflectionRuleTest.php
@@ -25,8 +25,7 @@ final class NoClassReflectionStaticReflectionRuleTest extends RuleTestCase
 
     public function provideData(): Iterator
     {
-        $errorMessage = NoClassReflectionStaticReflectionRule::ERROR_MESSAGE;
-        yield [__DIR__ . '/Fixture/NewOnExternal.php', [[$errorMessage, 13]]];
+        yield [__DIR__ . '/Fixture/NewOnExternal.php', [[NoClassReflectionStaticReflectionRule::ERROR_MESSAGE, 13]]];
 
         yield [__DIR__ . '/Fixture/SkipAllowedType.php', []];
         yield [__DIR__ . '/Fixture/SkipNonReflectionNew.php', []];
